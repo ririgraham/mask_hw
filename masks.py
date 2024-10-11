c40afd2f2109289cafa3e070fdb7e505ebf6c740
riri graham <rina.madatova@gmail.com>
2024-10-11 19:42:38 -0400

    add file1.py

--- masks.py
+++ masks.py
@@ -15,3 +15,5 @@ def get_mask_account(account_number: int) -> str:
 if __name__ == '__main__':
     print(get_mask_card_number(7000792289606361))
     print(get_mask_account(73654108430135874305))
+
+print('Hi')
