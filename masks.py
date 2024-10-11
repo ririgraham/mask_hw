c1d3d1fd706466e978cec90e9f29ba5b26a149db
riri graham <rina.madatova@gmail.com>
2024-10-11 19:32:35 -0400

    message

---
+++ masks.py
@@ -0,0 +1,17 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Transforming card number into str type"""
+    card_number = str(card_number)
+    """Formatting the card number"""
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+def get_mask_account(account_number: int) -> str:
+    """Transforming account number into str type"""
+    account_number = str(account_number)
+    """Formatting the account number"""
+    return f"**{account_number[-4:]}"
+
+
+if __name__ == '__main__':
+    print(get_mask_card_number(7000792289606361))
+    print(get_mask_account(73654108430135874305))
